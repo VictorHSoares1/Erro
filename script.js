1b689e25ccfcec4e05227bca74a37a892742d965
VictorHSoares1 <101873822+VictorHSoares1@users.noreply.github.com>
2022-12-11 17:23:49 +0000

    Ex10

---
+++ script.js
@@ -0,0 +1,16 @@
+let numeros = [10,20,30,66,38,22];
+let numerosPar;
+function retornaApenasNumerosPares(){
+ 
+  for (let i = 0; i < n; i++) {
+  if(num%2==0){
+   numerosPar.push(i);
+  
+  }
+   return numerosPar;
+  }
+
+ retornaApenasNumerosPares(numeros); 
+  
+}    
+
